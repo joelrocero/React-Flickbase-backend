5c8ccc0648cfa985b24b7a8661130f56bb22aab0
Joel.Rocero <joel.rocero@outlook.com>
2023-06-05 14:03:50 +0800

    Add Token Header in CORS

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ mongoose.connect(mongoUri, {
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
   next()
 })
