84086bfd2410366e9bd929a53ae68f6ba7882f30
Joel.Rocero <joel.rocero@outlook.com>
2023-06-05 13:56:48 +0800

    Add CORS

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ mongoose.connect(mongoUri, {
   useFindAndModify: false,
 })
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+  next()
+})
+
 app.use(bodyParser.json())
 app.use(checkToken)
 app.use('/api/users', users)
